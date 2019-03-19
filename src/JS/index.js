f4afa9259a8bfba0efa2dd2cac299810dd68665d
ana galvez <a.galvezsalas@gmail.com>
2019-03-19 14:48:31 -0300

    farebase inicializado

---
+++ src/JS/index.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyC6-D_wOl0xwxRoVaSQBrIKMBdHiU4BDu0",
+    authDomain: "banigualdad-d5784.firebaseapp.com",
+    databaseURL: "https://banigualdad-d5784.firebaseio.com",
+    projectId: "banigualdad-d5784",
+    storageBucket: "banigualdad-d5784.appspot.com",
+    messagingSenderId: "250712200354"
+  };
+  firebase.initializeApp(config);
