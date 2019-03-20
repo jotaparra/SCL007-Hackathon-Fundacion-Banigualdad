e66c8b953f9330d052ef1a6d633ce1bfd0446dc2
johannagoenagac <johannagoenaagc@gmail.com>
2019-03-20 14:46:10 -0300

    agregando pagina principal, aun sin terminar

--- src/JS/index.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: "AIzaSyC6-D_wOl0xwxRoVaSQBrIKMBdHiU4BDu0",
-    authDomain: "banigualdad-d5784.firebaseapp.com",
-    databaseURL: "https://banigualdad-d5784.firebaseio.com",
-    projectId: "banigualdad-d5784",
-    storageBucket: "banigualdad-d5784.appspot.com",
-    messagingSenderId: "250712200354"
-  };
-  firebase.initializeApp(config);
