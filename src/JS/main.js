7dcb9735ae090595355a0f392889476bc67d3929
johannagoenagac <johannagoenaagc@gmail.com>
2019-03-21 14:43:42 -0300

    Se agregan cards para mostrar productos

--- src/JS/main.js
+++ src/JS/main.js
@@ -33,48 +33,7 @@ document.getElementById('btnSignUp').addEventListener('click', signInWithEmailAn
 
 
 
-function showCards(data) {
-   const infoData= data;
 
-document.getElementById('root').innerHTML='';
-
-document.getElementById('product-list').innerHTML = '';
-   for (let i = 0; i < infoData.length; i++) {
-document.getElementById('product-list').innerHTML += `
       
-<!-- Card Wider -->
 
-<!-- Card Narrower -->
-      <div class="card card-cascade narrower">
-
-<!-- Card image -->
-         <div class="view view-cascade overlay">
-            <img  class="card-img-top" src="${data[i].img}" alt="Card image cap">
-               <a>
-                  <div class="mask rgba-white-slight"></div>
-               </a>
-         </div>
-
-<!-- Card content -->
-         <div class="card-body card-body-cascade">
-
-<!-- Label -->
-         <h5 class="pink-text pb-2 pt-1">${data.category}</h5>
-<!-- Title -->
-         <h4 class="font-weight-bold card-title">${data.name}</h4>
-             <p>${data.zone}</p>
-<!-- Text -->
-             <p class="card-text">${data.description}</p>
-<!-- Button -->
-            <a class="btn btn-unique">Ver contacto</a>
-
-         </div>
-
-         </div>
-<!-- Card Narrower -->`
-
-}
-
-}
-
-showCards(console.log())
+      
