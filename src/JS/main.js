1e0e40b1338198bfafac3d7be526080196aed675
ana galvez <a.galvezsalas@gmail.com>
2019-03-22 11:30:06 -0300

    timeline funcionando

--- src/JS/main.js
+++ src/JS/main.js
@@ -3,12 +3,18 @@ import {enviarConvalidacionAFirebase, readPost,guardandoComentarios, deletePost,
 window.onload = () =>{     
       checkAuthState((firebaseUser) => {
          if(firebaseUser){
-            // document.getElementById('loginRegister').style.display ="none";
-            // document.getElementById('btnLogout').style.display = "block"; 
+            document.getElementById('login').style.display ="none";
+            document.getElementById('timeline').style.display = "block"; 
+            document.getElementById('home').style.display='none';
+            document.getElementById('footer_page').style.display='block';
+            
+
+
             readPostFromDatabase();
             }else{
-            // document.getElementById('loginRegister').style.display ="block";
-            // document.getElementById('btnLogout').style.display = "none";
+            document.getElementById('home').style.display ="block";
+            document.getElementById('profile').style.display='none';
+            document.getElementById('footer_page').style.display='none'
          }   
       });
 
@@ -110,12 +116,12 @@ window.onload = () =>{
 
       document.getElementById('btnhome3').addEventListener('click', () =>{
       document.getElementById('home').style.display='block';
-      document.getElementById('handMade').style.display='none'
+      document.getElementById('foodCategory').style.display='none'
       })
 
       document.getElementById('btnhome4').addEventListener('click', () =>{
       document.getElementById('home').style.display='block';
-      document.getElementById('terapyAndBeauty').style.display='none'
+      document.getElementById('handMade').style.display='none'
       })
 
    }
@@ -129,7 +135,7 @@ const registerWithEmailAndPassword = () => {
    registerUser(emailUser, passwordUser);
 
 };
-document.getElementById('registerButton').addEventListener('click', registerWithEmailAndPassword);
+// document.getElementById('registerButton').addEventListener('click', registerWithEmailAndPassword);
 
 
 //Iniciar Sesión correo y contraseña
